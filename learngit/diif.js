0ff852004d839d65db56041cc768976ca289af99
wali ansari <waliansari40@gmail.com>
2022-10-02 01:37:17 +0530

    aster

---
+++ learngit/diif.js
@@ -0,0 +1,13 @@
+  let cities = ["Mumbai","DeLhi","LUcknow"];
+ for(i=0;i<cities.length;i++){
+let lowercase = cities.map(cities => cities.toLowerCase())
+console.log(lowercase);
+ }
+
+//  let cities  = ['mumbai','Delhi','KolKata']
+// for(i=0;i<cities.length;i++){
+// let lowercase = cities[i].toLocaleLowerCase()
+// let uppercase = lowercase.slice(0,1);
+// lowercase = lowercase.replace(lowercase,lowercase.toUpperCase());
+// console.log(uppercase);
+// }
